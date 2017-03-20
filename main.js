73a87eeac4419b5d2332db58df93938e095d6b64
Taylor McArthur <taydm.erek@gmail.com>
2017-03-19 17:31:48 -0700

    added js

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
 	$.getJSON('https://api.etsy.com/v2/listings/active.js?api_key=h9oq2yf3twf4ziejn10b717i&keywords=whiskey&includes=Images,Shop&callback=?', function(data){
 	console.log(data)
+	}).then(function(items){
+		var imgs = items.results.map(function(img){
+			return {
+				title: img.title.substring(0, 30) + "....",
+				price: img.price,
+				shopname: img.Shop.shop_name,
+				image: img.Images[0].url_170x135
+			}
+		})
+		var htmlStr = `<ul class="imgData">`
+		imgs.forEach(function(val) {
+			htmlStr += `
+			<li>
+			<img src="${val.image}"/>
+			<p>${val.title}</p>
+			<p>${val.shopname}</p>
+			<p>${val.price}</p>
+			</li>`
+		})
+		$("#apiShit").html(htmlStr)
 	})
+
 	$("#right").on("click",function(){
 		var ansYes = confirm("are you sure?") 
 		if (ansYes === true) {
@@ -10,5 +31,7 @@ $(document).ready(function(){
 			alert("Bye Felicia")
 		}
 	}) 
+
+	//img, description, shop name, price (just map it)
 	
 })
